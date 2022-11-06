56e8c428775a04f7ad47c9662ecf5594395ca4e0
Yohann Bethoule <yoyoteva@gmail.com>
2022-11-06 23:00:03 +0100

    Added font and places filter

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const map = L.map('map', {
+    crs: L.CRS.Simple,
+    minZoom: -2
+});
+
+const bounds = [[0, 0], [4334, 5000]];
+const image = L.imageOverlay('assets/map.webp', bounds).addTo(map);
+
+map.fitBounds(bounds);
+
+let markersData = [];
+
 const createInfoDialog = (data) => {
     let info = ``;
     if (data.title) {
@@ -16,34 +28,82 @@ const createInfoDialog = (data) => {
     return info;
 }
 
+const getFilters = () => {
+    let filters = {
+        'places': []
+    };
+    document.querySelectorAll('#filters fieldset').forEach(category => {
+        category.querySelectorAll('input[type=checkbox]:checked').forEach(filter => {
+            filters[filter.dataset.category].push(filter.dataset.filter);
+        })
+    });
+    return filters;
+}
+
+const clearMarkers = () => {
+    map.eachLayer(function (layer) {
+        if (layer instanceof L.Marker) {
+            map.removeLayer(layer)
+        }
+    })
+}
+
+const renderMarkersFromFilters = (filters) => {
+    clearMarkers();
+    let isRendered = false;
+    let markers = L.layerGroup();
+    let marker;
+    for (const m of markersData) {
+        isRendered = false;
+        for (const category of Object.keys(filters)) {
+            for (const filter of filters[category]) {
+                if (m.tags[category].includes(filter)) {
+                    isRendered = true;
+                }
+            }
+        }
+        if (isRendered) {
+            marker = createMarker(map, m);
+            markers.addLayer(marker);
+        }
+    }
+    map.addLayer(markers);
+}
+
+const onFilterChange = (e) => {
+    const element = e.target;
+    if (element.dataset.filter === 'all') {
+        document.querySelectorAll(`#filters-${element.dataset.category} input[type=checkbox]:not([data-filter=all])`).forEach(el => {
+            if (element.checked) {
+                el.checked = true;
+                el.disabled = true;
+            } else {
+                el.checked = false;
+                el.disabled = false;
+            }
+        })
+    }
+    renderMarkersFromFilters(getFilters());
+}
+
 const createMarker = (map, data) => {
-    const sol = L.latLng([ 4334-data.y, data.x ]);
-    L.marker(sol, {
+    const sol = L.latLng([4334 - data.y, data.x]);
+    let marker = L.marker(sol, {
         title: data.title,
         alt: data.title,
         opacity: 0.8
-    }).addTo(map).bindPopup(createInfoDialog(data))
+    })
+    marker.bindPopup(createInfoDialog(data))
+    return marker;
 }
-
-const map = L.map('map', {
-    crs: L.CRS.Simple,
-    minZoom: -2
-});
-
-const bounds = [[0, 0], [4334, 5000]];
-const image = L.imageOverlay('assets/map.webp', bounds).addTo(map);
-
-map.fitBounds(bounds);
 
 fetch('./markers.json')
     .then((response) => response.json())
     .then((data) => {
-        console.log(data)
-        for (let d of data) {
-            console.log(d)
-            createMarker(map, d)
-       }
+        markersData = data;
+        renderMarkersFromFilters(getFilters());
     });
 
-
-
+document.querySelectorAll('#filters input[type=checkbox]').forEach(element => {
+    element.addEventListener('change', onFilterChange);
+});
